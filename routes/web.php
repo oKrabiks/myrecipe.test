317c632d36541af1fa16f6750f912129ef71aebb
oKrabiks <ovaselis@gmail.com>
2025-05-10 10:00:25 +0300

    Atjauninu routes, lai pareizi strādātu ar receptēm, kategorijām, autentifikāciju utt.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\KeywordController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Galvenā lapa 
+Route::get('/', [RecipeController::class, 'index'])->name('home');
+
+// Receptes
+Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.index');
+Route::get('/recipes/create', [RecipeController::class, 'create'])->name('recipes.create');
+Route::post('/recipes', [RecipeController::class, 'store'])->name('recipes.store');
+Route::get('/recipes/{recipe}', [RecipeController::class, 'show'])->name('recipes.show');
+Route::get('/recipes/{recipe}/edit', [RecipeController::class, 'edit'])->name('recipes.edit');
+Route::put('/recipes/{recipe}', [RecipeController::class, 'update'])->name('recipes.update');
+Route::delete('/recipes/{recipe}', [RecipeController::class, 'destroy'])->name('recipes.destroy');
+
+// Kategorijas
+Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+
+// Atslēgvārdi
+Route::get('/keywords/{keyword}', [KeywordController::class, 'show'])->name('keywords.show');
+
+//Laravel UI
+Auth::routes();
+
+//API
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
