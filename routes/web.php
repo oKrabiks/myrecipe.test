1c01621aa8268355a2b0dfdb390b70f934b2b288
oKrabiks <ovaselis@gmail.com>
2025-05-26 21:32:32 +0300

    Ieviešu galvenās funkcionalitātes routes

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,9 @@
 use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\KeywordController;
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 // Galvenā lapa 
 Route::get('/', [RecipeController::class, 'index'])->name('home');
@@ -36,10 +25,3 @@ Route::get('/categories/{category}', [CategoryController::class, 'show'])->name(
 // Atslēgvārdi
 Route::get('/keywords/{keyword}', [KeywordController::class, 'show'])->name('keywords.show');
 
-//Laravel UI
-Auth::routes();
-
-//API
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
