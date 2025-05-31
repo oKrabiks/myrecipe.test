fc5e1ab256aba43170dd8d066a01157e235a78b4
oKrabiks <ovaselis@gmail.com>
2025-05-31 14:11:24 +0300

    Papildinu skatus, lai tie strādātu ar autentifikāciju

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,36 +1,4 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $recipe->title }}</title>
-    </head>
-<body>
-    <h1>{{ $recipe->title }}</h1>
 
-    <div class="recipe-details">
-        <p>Kategorija: <a href="{{ route('categories.show', $recipe->category->id) }}">{{ $recipe->category->name }}</a></p>
-        <p>Autors: {{ $recipe->user->name }}</p>
-        @if ($recipe->photo)
-            <img src="{{ asset('storage/recipes/' . $recipe->photo) }}" alt="{{ $recipe->title }}" width="400">
-        @endif
-        <p>Sastāvdaļas:</p>
-        <pre>{{ $recipe->ingredients }}</pre>
-        <p>Pagatavošanas Soļi:</p>
-        <pre>{{ $recipe->steps }}</pre>
-
-        <p>Atslēgvārdi:
-            @foreach ($recipe->keywords as $keyword)
-            <span class="badge bg-secondary">{{ $keyword->name }}</span>
-            @endforeach
-        </p>
-    </div>
-
-    <a href="{{ route('recipes.index') }}">Atpakaļ uz receptēm</a>
-    </body>
-</html>
-=======
 @extends('layouts.app')
 
 @section('title', $recipe->title)
@@ -78,4 +46,4 @@
 
     </style>
 @endpush
->>>>>>> cc9eebe (Ieviesta autentifikācija, atjaunoju routes, nav bar pievienoju pogas lai pieteiktos, reģistrētos, atteiktos)
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\KeywordController;
